282978f80f5b559edef5187bb6bc4e24fd42fe3a
Parikhit <parikhitritzz@gmail.com>
2022-12-07 21:22:14 +0530

    Final Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+function About() {
+    return (
+        <>
+            <h1 className='text-6xl text-primary mb-4 '>Github Profile App</h1>
+
+            <p className='text-lg text-gray-400'>
+                Version <span className='text-white'>1.0.0</span>
+            </p>
+            <p className='text-lg text-gray-400'>
+                Created By:
+                <a className='text-white'>Parikhit Baruah</a>
+            </p>
+        </>
+    );
+}
+
+export default About;
